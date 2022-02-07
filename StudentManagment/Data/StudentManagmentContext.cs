bda8fd7e9bdd084677baf129280a5e1f2c3538b2
said <js2907846@gmail.com>
2022-02-07 23:32:57 +0100

    feat : crud methods were implemented within sql database

---
+++ StudentManagment/Data/StudentManagmentContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StudentManagment.Models;
+
+namespace StudentManagment.Data
+{
+    public class StudentManagmentContext : DbContext
+    {
+        public StudentManagmentContext (DbContextOptions<StudentManagmentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StudentManagment.Models.Student> Student { get; set; }
+    }
+}
---
+++ StudentManagment/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagment.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+        public string Subject { get; set; }
+
+        public Student()
+        {
+
+        }
+    }
+}
